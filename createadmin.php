278288bcc26b5d8e1c624292534ceafa5c304929
MohamedAshraf176 <mohamed230105239@sut.edu.eg>
2024-12-28 22:21:43 +0200

    Update createadmin.php

--- createadmin.php
+++ createadmin.php
@@ -3,25 +3,25 @@ try {
     $pdo = new PDO("mysql:host=localhost;dbname=university_transportation", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $password = "Student123";
+    $password = "admin123";
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     $sql = "INSERT INTO users (FullName, Email, PhoneNumber, UserType, PasswordHash, DateJoined) 
             VALUES (
                 'Mohaned User', 
-                'student@university.edu', 
+                'admin@university.edu', 
                 '1234567890', 
-                'Student', 
+                'Administrator', 
                 :hashedPassword,
                 CURRENT_TIMESTAMP
             )";
-    
+
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':hashedPassword', $hashedPassword);
     $stmt->execute();
 
     echo "Admin user created successfully!";
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 ?>
